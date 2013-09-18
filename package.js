361f332e53e30f7b0d8e47af46c66ddffb2098ad
Morten Henriksen <mh@gi2.dk>
2013-09-18 13:14:17 +0200

    Bump version 0.0.5

--- package.js
+++ package.js
@@ -1,5 +1,6 @@
 Package.describe({
-    summary: '\u001b[32mv0.0.4\n'+
+    summary: 'Adds HTTP.methods\n'
+         '\u001b[32mv0.0.5\n'+
          '\u001b[33m-----------------------------------------\n'+
          '\u001b[0m Adds HTTP.methods                        \n'+
          '\u001b[0m                                          \n'+
