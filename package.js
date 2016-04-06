585f4ec8651e9729c2970208fd043be47b0ae7f9
Morten Henriksen <mh@gi2.dk>
2016-04-06 13:13:43 +0200

    Release 0.0.31

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   git: 'https://github.com/CollectionFS/Meteor-http-methods.git',
   name: 'cfs:http-methods',
-  version: '0.0.30',
+  version: '0.0.31',
   summary: 'Adds HTTP.methods RESTful'
 });
 
